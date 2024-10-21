1254d63a43aec926f0f5c61d70ac08c00e2b0efd
evertzner <esteban.vertzner@gmail.com>
2024-10-21 12:26:26 +0200

    misc

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      screens: {
+        '2xl': '1440px'
+      },
       fontFamily: {
         antonio: ['Antonio', 'sans-serif'],
         spartan: ['League Spartan', 'sans-serif']
