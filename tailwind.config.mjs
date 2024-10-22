5b2a9b53a7603da7cf720516f86f97e51e58ab86
Esteban Vertzner <esteban.vertzner@canda.com>
2024-10-22 19:59:14 +0200

    view transitions. tablet. mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  safelist: [
+    'border-[#419EBB]',
+    'bg-[#419EBB]',
+    'bg-[#DEF4FC]',
+    'border-[#EDA249]',
+    'bg-[#EDA249]',
+    'bg-[#F7CC7F]',
+    'border-[#6D2ED5]',
+    'bg-[#6D2ED5]',
+    'bg-[#545BFE]',
+    'border-[#1EC1A2]',
+    'bg-[#1EC1A2]',
+    'bg-[#65F0D5]',
+    'border-[#D83A34]',
+    'bg-[#D83A34]',
+    'bg-[#ECAD7A]',
+    'border-[#CD5120]',
+    'bg-[#CD5120]',
+    'bg-[#FCCB6B]',
+    'border-[#D14C32]',
+    'bg-[#D14C32]',
+    'bg-[#FF6A45]',
+    'border-[#2D68F0]',
+    'bg-[#2D68F0]',
+    'bg-[#497EFA]'
+  ],
   theme: {
     extend: {
       screens: {
@@ -11,31 +37,7 @@ export default {
         spartan: ['League Spartan', 'sans-serif']
       },
       colors: {
-        blue: {
-          DEFAULT: '#2D68F0'
-        },
-        'dark-blue': '#070724',
-        'light-blue': '#419EBB',
-        gray: {
-          DEFAULT: '#838391'
-        },
-        'dark-gray': '#38384F',
-        yellow: {
-          DEFAULT: '#EDA249'
-        },
-        orange: {
-          DEFAULT: '#CD5120'
-        },
-        'orange-red': '#D14C32',
-        purple: {
-          DEFAULT: '#6D2ED5'
-        },
-        turquoise: {
-          DEFAULT: '#1EC1A2'
-        },
-        red: {
-          DEFAULT: '#D83A34'
-        }
+        'dark-blue': '#070724'
       },
       spacing: {
         13: '3.25rem'
